1ef371671e9e1d3ed0f4a5448369a0d0142a50d6
houweikang <373496706@qq.com>
2020-02-21 20:44:21 +0800

    function

---
+++ func01.py
@@ -0,0 +1,13 @@
+# pycharm
+# 格式化快捷键 Ctrl + shift + F
+# 快速提示 alt + enter
+import random
+
+
+def generate_random():
+    for i in range(10):
+        ran = random.randint(1, 20)
+        print(ran)
+
+
+generate_random()
